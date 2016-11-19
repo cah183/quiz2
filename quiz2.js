d98bbd7ed0e130371a16940275d57c91e8063460
cah183 <calvinham96@gmail.com>
2016-11-19 06:56:09 -0800

    Update quiz2.js

--- quiz2.js
+++ quiz2.js
@@ -4,12 +4,54 @@
 //  - create a button in the html that, once clicked, will fire off the function described in the next buller point
 //  - write a function that will fade out all of the html inside the body tag and replace it with a custom goodbye message when you click the button
 //  - finally, write a function that makes you happy :D (as in, have some fun a write something cool, it's up to you!)
-
 (function($){
+	$('#panel').hide();
+	var list = document.getElementById('secondarySection');
+	var items = list.getElementsByTagName('li')
+	console.log(items);
+	//change ol
+	for(var i = 0; i < items.length; i++){
+    var input=items[i];
+ 	input.innerHTML='Uniformity, is and always will be the prefered choice for the hive.';
+	input.style.color = 'red';
+	console.log(input);
+}
+
 	$('.links li a').on('click', function(e) {
 		e.preventDefault()
-		alrt('you clicked a link, good for you');
+		alrt('you just clicked a link, goodjob');
 	});
 
-	console.log("this is a message for you!!!")
-})(jQery);
+}(jQuery));
+
+//Shows my accordion
+function show(){
+	if(accCount == 0){
+		$('#panel').show();
+ 		accCount = 1;
+	}
+ 	else{
+ 		$('#panel').hide();
+ 		accCount = 0;
+ }
+
+} 
+//Fade out function
+function quizzy(){
+
+ 	setTimeout(function(){
+ 	  $(document.body).fadeOut('slow');
+ 	},1000)
+ 
+ 	setTimeout(function(){
+ 		alert('Your custom bye message "peace" .')
+ 	},3000)
+ }
+ 
+//My XkCD function
+function funFunc(){
+ 	console.log("You have initiated randomizer2000");
+ 	console.log("Generating a number at random");
+	console.log("Your random number is 4");
+
+}
